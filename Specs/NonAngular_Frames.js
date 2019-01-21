7456e52d41df6d00f3eeb32cf2dfccdd80e4be38
Unknown <jonan.naguit@gmail.com>
2019-01-21 20:50:12 +1300

    iFrame handling added

---
+++ Specs/NonAngular_Frames.js
@@ -0,0 +1,18 @@
+describe('Protractor Alert steps',function()
+{  
+	it('Open NonAngular js website Alerts',function()
+	{
+		//non-angular site
+		browser.waitForAngularEnabled(false);
+		//maximize window
+		browser.driver.manage().window().maximize();
+		browser.get("http://qaclickacademy.com/practice.php");
+		//switch focus to iframe
+		browser.switchTo().frame("courses-iframe");
+		//get and display sign_in text
+		element(by.css("a[href*='sign_in']")).getText().then(function(text)
+		{
+			console.log(text);
+		})
+	})
+})
