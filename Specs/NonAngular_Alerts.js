5bd3d60f78e8272d5f8e02f95a6bee34a4340ba1
Unknown <jonan.naguit@gmail.com>
2019-01-21 20:26:28 +1300

    Non Angular Alerts added

---
+++ Specs/NonAngular_Alerts.js
@@ -0,0 +1,13 @@
+describe('Protractor Alert steps',function()
+{  
+	it('Open NonAngular js website Alerts',function()
+	{
+		browser.waitForAngularEnabled(false);
+		browser.get("http://qaclickacademy.com/practice.php");
+		element(by.id("confirmbtn")).click();
+		browser.switchTo().alert().dismiss().then(function()
+		{
+			browser.sleep(5000);
+		})
+	})
+})
